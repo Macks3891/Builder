9c053d9e3b81fd4b555d4d00de3a8450fb56bf90
Максим <166473866+Macks3891@users.noreply.github.com>
2024-09-21 17:28:28 +0500

    Builder 1 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String str = "HellolleH";
+        if (isPalindromeUsingStringBuilder(str)) {
+            System.out.println("Палиндром");
+        } else {
+            System.out.println("Не палиндром");
         }
     }
+
+    public static boolean isPalindromeUsingStringBuilder(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        String reversedStr = sb.reverse().toString();
+        return str.equals(reversedStr);
+    }
 }
+
